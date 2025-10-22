7809864841f86608dd456007f26e0615cbc03cda
Shiva Pandit <shivakrwpa1@gmail.com>
2025-10-22 21:56:15 +0530

    ✨ Setup Express.js + EJS + File System integration
    - Initialized Express server with JSON, URL parser, and static file setup
    - Configured EJS as the view engine for dynamic rendering
    - Implemented route to read files from ./files directory using fs.readdir()
    - Prepared base structure for a Note-Taking Web App

---
+++ 7_Revisiting_key_Concepts/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index");
+  });
+});
+
+app.listen(3000);
