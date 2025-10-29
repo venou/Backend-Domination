72415c7c7f774cfdc9303a3ba8c29763d00cb917
Shiva Pandit <shivakrwpa1@gmail.com>
2025-10-29 18:58:19 +0530

    completed and tested full CRUD functionality with create, read, update, and delete routes

--- 10_CRUD_operations_with_ejs_Server_Side_Rendering/index.js
+++ 10_CRUD_operations_with_ejs_Server_Side_Rendering/index.js
@@ -2,11 +2,11 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const userModel = require("./Models/users");
+
 app.set("view engine", "ejs");
 
-app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -14,18 +14,26 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/read", (req, res) => {
-  res.render("read");
+app.get("/read", async (req, res) => {
+  let allUsers = await userModel.find();
+  res.render("read", { users: allUsers });
 });
 
 app.post("/create", async (req, res) => {
+  console.log(req.body);
   let { name, email, image } = req.body;
+
   let createdUser = await userModel.create({
     name: name,
     email: email,
     image: image,
   });
-  res.send(createdUser);
+  res.redirect("/read");
+});
+
+app.get("/delete/:id", async (req, res) => {
+  let deleteuser = await userModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect("/read");
 });
 
 app.listen(3000);
