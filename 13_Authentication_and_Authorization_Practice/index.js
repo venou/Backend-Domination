1e0e5a9e8f9b975f18db8db3b086e490cc66c4a7
Shiva Pandit <shivakrwpa1@gmail.com>
2025-11-04 23:39:51 +0530

    Practice Authentication and Authorization

--- 13_Authentication_and_Authorization_Practice/index.js
+++ 13_Authentication_and_Authorization_Practice/index.js
@@ -6,17 +6,14 @@ const userModel = require("./models/user");
 const { name } = require("ejs");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-
 app.get("/", (req, res) => {
   res.render("index");
 });
-
 app.post("/create", (req, res) => {
   let { username, email, password, age } = req.body;
   bcrypt.genSalt(10, (err, salt) => {
@@ -33,24 +30,17 @@ app.post("/create", (req, res) => {
     });
   });
 });
-
 app.get("/login", (req, res) => {
   res.render("login");
 });
-
 app.post("/login", async (req, res) => {
   let user = await userModel.findOne({ email: req.body.email });
-  if (!user) return res.send("Something went wrong");
-
-  bcrypt.compare(req.body.password, user.password, (err, result) => {
-    if (result) res.send("Logged In");
-    else res.send("Something went wrong");
-  });
+  if (!user) {
+    return res.send("Wrong Input");
+  }
 });
-
 app.get("/logout", (req, res) => {
   res.cookie("token", "");
   res.redirect("/");
 });
-
 app.listen(3000);
---
+++ 14_Practice/app.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000);
