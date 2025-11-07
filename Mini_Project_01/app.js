a4298470eeacb30f25a897be13c5dd292ec411d8
Shiva Pandit <shivakrwpa1@gmail.com>
2025-11-07 23:52:20 +0530

    feat: setup mini backend project with user-post relationship
    - Initialized Express server and MongoDB connection
    - Created User and Post mongoose models
    - Implemented routes to create users and posts
    - Established one-to-many relationship between User and Post

---
+++ Mini_Project_01/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", async (req, res) => {
+  let { email, username, age, name, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(500).send("User Already Registered");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password: hash,
+      });
+      let token = jwt.sign({ email: email, userId: user._id }, "SecretKey");
+      res.cookie("token", token);
+      res.send("registered");
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (!user) return res.status(500).send("Something went Wrong");
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) res.status(200).send("you can login");
+    else res.redirect("/login");
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
+
+app.listen(3000);
