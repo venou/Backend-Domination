3cac1533ea3409546077cc0e89847adf81b14f00
Shiva Pandit <shivakrwpa1@gmail.com>
2025-11-11 23:51:21 +0530

    add complete profile UI and fix EJS post rendering

--- Mini_Project_01/app.js
+++ Mini_Project_01/app.js
@@ -19,9 +19,23 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/profile", isLoggedIn, (req, res) => {
-  console.log(req.user);
-  res.render("login");
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("posts");
+  res.render("profile", { user });
+});
+
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+  let post = await postModel.create({
+    user: user._id,
+    content,
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
 });
 
 app.post("/register", async (req, res) => {
@@ -54,7 +68,7 @@ app.post("/login", async (req, res) => {
     if (result) {
       let token = jwt.sign({ email: email, userId: user._id }, "SecretKey");
       res.cookie("token", token);
-      res.status(200).send("you can login");
+      res.status(200).redirect("/profile");
     } else res.redirect("/login");
   });
 });
@@ -65,7 +79,7 @@ app.get("/logout", (req, res) => {
 });
 
 function isLoggedIn(req, res, next) {
-  if (req.cookies.token === "") res.send("you must be logged in");
+  if (req.cookies.token === "") res.redirect("/login");
   else {
     let data = jwt.verify(req.cookies.token, "SecretKey");
     req.user = data;
--- Mini_Project_01/models/user.js
+++ Mini_Project_01/models/user.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
   age: Number,
   email: String,
   password: String,
-  posts: [{ type: mongoose.Schema.Types.ObjectId }],
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
 });
 
 module.exports = mongoose.model("user", userSchema);
